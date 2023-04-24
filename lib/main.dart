bbae344878725ffa71766757d523d21df22eca53
Tim <timothy.lawless@gmail.com>
2023-04-23 21:56:07 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flexible Grid of Cards'),
+      ),
+      body: SizedBox(
+        width: double.infinity,
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: MediaQuery.of(context).size.width > 500 ? 3 : 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          ),
+          itemBuilder: (context, index) {
+            return Card(
+              elevation: 3,
+              color: Colors.primaries[index],
+              child: Center(
+                child: Text(
+                  "Card ${index + 1}",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            );
+          },
+          itemCount: 12,
+        ),
+      ),
+    );
+  }
+}
